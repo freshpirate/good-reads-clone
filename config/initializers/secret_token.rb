f5a3d625fc16a4689a283094d88e719410dea196
Shubham Sahu <shubham.sahu@freshworks.com>
2018-05-28 13:23:59 +0530

    Base commit: Add base structure to landing page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoodReads::Application.config.secret_token = '2bcee0955f8912db140c905f0a833b4b058ef2849e2375a904eea5417a0b0cc3e6835fbcdbe2b8383e5f3b4b20240f39b5e68bc3993d6578c7ca2d3e150e9ff0'
